99a289c388b7efff7d3ca0db9c7400abc2700552
Dan Gochuico <79511016+TheDetective14@users.noreply.github.com>
2024-10-28 15:57:44 +0800

    WORKING FINALLY

---
+++ code/main.py
@@ -0,0 +1,52 @@
+from settings import *
+from sprites import *
+from player import Player
+from groups import AllSprites
+from pytmx.util_pygame import load_pygame
+
+class Game:
+    def __init__(self):
+        # Setup
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Library Escape")
+        pygame.mixer.init()
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
+        self.enemy_sprites = pygame.sprite.Group()
+
+        self.music = pygame.mixer.Sound(join('audio', 'Background Music.mp3'))
+        self.music.play(loops = -1)
+
+        self.setup()
+    
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.all_sprites.update(dt)
+
+            self.display_surface.fill('black')            
+            self.all_sprites.draw(self.player.rect.center)
+            pygame.display.update()
+            
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ data/tilesets/world_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="world_tileset" tilewidth="16" tileheight="16" tilecount="4158" columns="66">
+ <image source="../graphics/tilesets/world_tileset.png" width="1056" height="1023"/>
+</tileset>
